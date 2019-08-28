4060a9d93094d236ad5f1afcbc7fdeb69406e10f
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-08-28 16:38:32 -0500

    Section 14 - LAB - Class Vehicle - Method Break - done

--- Section 14/lab/Vehicle.js
+++ Section 14/lab/Vehicle.js
@@ -7,33 +7,41 @@ class Vehicle {
     this._direction = direction;
     this._currentSpeed = currentSpeed;
     this._topSpeed = topSpeed;
-    this._engineStarted = engineStarted;    
+    this._engineStarted = engineStarted;
+    this._incrementSpeed = 10; // km per hour
   }
 
   // methods
   accelerate() {
     if (this._engineStarted) {
-      let increment = 10; // km/h
       if ((this._currentSpeed + increment) >= this._topSpeed) {
         this._currentSpeed = this._topSpeed;
       } else {
         this._currentSpeed += increment;
       }
     } else {
-      alert('Please Turn On Vehicle');
+      alert('Please turn on Vehicle first');
     }
   }
 
   break() {
-
+    if (this._engineStarted) {
+      if (this._currentSpeed > 0) {
+        this._currentSpeed -= increment;
+      } else {
+        alert('Current vehicle\'s speed is zero, please gain some speed first before breaking')
+      }
+    } else {
+      alert('Please turn on Vehicle first');
+    }
   }
 
   turnOn() {
-
+    this._engineStarted = true;
   }
 
   turnOff() {
-
+    this._engineStarted = false;
   }
 
   turnLeft() {
