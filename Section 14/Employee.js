2c6b48c332e06137455482a08a2d2d36bdd3f895
Jorge Salinas <jorge.salinas.jsf@outlook.com>
2019-08-27 22:26:18 -0500

    JavaScript Classes - Definig a New Class

---
+++ Section 14/Employee.js
@@ -0,0 +1,11 @@
+class Employee {
+  // constructor
+  constructor(firstName, lastName, social, jobTitle, salary) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._social = social;
+    this._jobTitle = jobTitle;
+    this._salary = salary;
+    this._active = true;  // default value
+  }
+}
